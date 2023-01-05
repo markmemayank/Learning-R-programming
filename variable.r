357c24d0f1f0cb244a84f53e2b12097e083df079
Mayank Kumar <96015005+markmemayank@users.noreply.github.com>
2023-01-05 15:10:34 +0530

    Update variable.r

--- variable.r
+++ variable.r
@@ -16,3 +16,19 @@ var1 <- var2 <- var3 <- "Orange"
 var1
 var2
 var3
+
+# Legal variable names:
+myvar <- "John"
+my_var <- "John"
+myVar <- "John"
+MYVAR <- "John"
+myvar2 <- "John"
+.myvar <- "John"
+
+# Illegal variable names:
+2myvar <- "John"
+my-var <- "John"
+my var <- "John"
+_my_var <- "John"
+my_v@ar <- "John"
+TRUE <- "John"
