2ea4d204b82e7a8f3c181561c65d242938243d14
Mayank Kumar <96015005+markmemayank@users.noreply.github.com>
2023-01-13 22:35:11 +0530

    Add files via upload

---
+++ 21 Factors.r
@@ -0,0 +1,7 @@
+# An array with one dimension with values ranging from 1 to 24
+thisarray <- c(1:24)
+thisarray
+
+# An array with more than one dimension
+multiarray <- array(thisarray, dim = c(4, 3, 2))
+multiarray
