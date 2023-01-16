9c2ebd6a70ddd60da6939d1f71b69565f452d5f1
Mayank Kumar <96015005+markmemayank@users.noreply.github.com>
2023-01-16 23:32:59 +0530

    Update 18 Matrices.r

--- 18 Matrices.r
+++ 18 Matrices.r
@@ -1,10 +1,13 @@
 # A matrix can be created with the matrix() function. Specify the nrow and ncol parameters to get the amount of rows and columns:
 
+
 # Create a matrix
 thismatrix <- matrix(c(1,2,3,4,5,6), nrow = 3, ncol = 2)
 
+
 # Print the matrix
 thismatrix
+
 
 #Access Matrix Items
 
@@ -12,10 +15,12 @@ thismatrix <- matrix(c("apple", "banana", "cherry", "orange"), nrow = 2, ncol = 
 thismatrix
 thismatrix[1, 2]
 
+
 # Accessing the Second row
 thismatrix <- matrix(c("apple", "banana", "cherry", "orange"), nrow = 2, ncol = 2)
 
 thismatrix[2,]
+
 
 # More than one row can be accessed if you use the c() function:
 
@@ -23,7 +28,22 @@ thismatrix <- matrix(c("apple", "banana", "cherry", "orange","grape", "pineapple
 
 thismatrix[c(1,2),]
 
+
 # More than one column can be accessed if you use the c() function:
 thismatrix <- matrix(c("apple", "banana", "cherry", "orange","grape", "pineapple", "pear", "melon", "fig"), nrow = 3, ncol = 3)
 
 thismatrix[, c(1,2)]
+
+
+# Use the cbind() function to add additional columns in a Matrix:
+thismatrix <- matrix(c("apple", "banana", "cherry", "orange","grape", "pineapple", "pear", "melon", "fig"), nrow = 3, ncol = 3)
+
+newmatrix <- cbind(thismatrix, c("strawberry", "blueberry", "raspberry"))
+
+newmatrix
+
+
+# 
+
+
+
