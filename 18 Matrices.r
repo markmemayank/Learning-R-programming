68bf77f03bd52977136979b3515a0b183064f12e
Mayank Kumar <96015005+markmemayank@users.noreply.github.com>
2023-01-16 23:29:22 +0530

    Update 18 Matrices.r

--- 18 Matrices.r
+++ 18 Matrices.r
@@ -23,3 +23,7 @@ thismatrix <- matrix(c("apple", "banana", "cherry", "orange","grape", "pineapple
 
 thismatrix[c(1,2),]
 
+# More than one column can be accessed if you use the c() function:
+thismatrix <- matrix(c("apple", "banana", "cherry", "orange","grape", "pineapple", "pear", "melon", "fig"), nrow = 3, ncol = 3)
+
+thismatrix[, c(1,2)]
