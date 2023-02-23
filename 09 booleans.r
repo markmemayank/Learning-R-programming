2fbd2029197e725a0bfa160335747e11884dc40d
Mayank Kumar <96015005+markmemayank@users.noreply.github.com>
2023-02-23 22:23:01 +0530

    Update 09 booleans.r

--- 09 booleans.r
+++ 09 booleans.r
@@ -1,7 +1,7 @@
 # TRUE OR FALSE (1 or 0)
-10 > 9    # TRUE because 10 is greater than 9
-10 == 9   # FALSE because 10 is not equal to 9
-10 < 9    # FALSE because 10 is greater than 9
+10 > 9    # It is TRUE because 10 is greater than 9
+10 == 9   # It is FALSE because 10 is not equal to 9
+10 < 9    # It is FALSE because 10 is greater than 9
 
 # comparing two variables
 a <- 6
